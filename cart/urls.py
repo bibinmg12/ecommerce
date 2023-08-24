e1f6e054fa41eb6249459f3f3986d583037763c4
bibinmg12 <bibinmg12@gmail.com>
2023-08-24 14:21:43 +0530

    blender_first_commit

---
+++ cart/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+app_name='cart'
+urlpatterns = [
+    path('add/<int:product_id>/',views.add_cart,name='add_cart'),
+    path('',views.cart_details,name='cart_details'),
+    path('remove/<int:id>/',views.cart_remove,name='cart_remove'),
+    path('full_remove/<int:id>/',views.full_remove,name='full_remove'),
+]
